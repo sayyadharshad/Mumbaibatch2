b266e8716219e3ce21b8d4385c42b7cdf1c3d143
krutika321 <51986886+krutika321@users.noreply.github.com>
2019-07-09 15:35:42 +0530

    Add files via upload

---
+++ Teams/krutika/index.php
@@ -0,0 +1,47 @@
+<?php
+$dbHost     = "localhost";
+$dbUsername = "krutika";
+$dbPassword = "krutika123";
+$dbName     = "Photos";
+
+// Create database connection
+$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+// Check connection
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+
+
+}  // Initialize message variable
+  $msg = "";
+
+  // If upload button is clicked ...
+  if (isset($_POST['upload'])) {
+  	// Get image name
+  	$image = $_FILES['image']['name'];
+  	// Get text
+  	$image_text = mysqli_real_escape_string($db, $_POST['image_text']);
+
+  	// image file directory
+  	$target = "images/".basename($images);
+
+  	$sql = "INSERT INTO images (image, image_text) VALUES ('$image', '$image_text')";
+  	// execute query
+  	mysqli_query($db, $sql);
+
+  	if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+  		$msg = "Image uploaded successfully";
+  	}else{
+  		$msg = "Failed to upload image";
+  	}
+  }
+  $result = mysqli_query($db, "SELECT * FROM images");
+?>
+<?php
+  while ($row = mysqli_fetch_array($result)) {
+    echo "<div id='img_div'>";
+      echo "<img src='images/".$row['image']."' >";
+      echo "<p>".$row['image_text']."</p>";
+    echo "</div>";
+  }
+?>
